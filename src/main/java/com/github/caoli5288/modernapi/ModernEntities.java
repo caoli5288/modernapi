6668658de7851030a98a09898ced4f7ea0ab818d
caoli5288 <caoli5288@gmail.com>
2025-08-30 13:29:52 +0800

    feat(entities): add methods to get and set absorption amount

--- src/main/java/com/github/caoli5288/modernapi/ModernEntities.java
+++ src/main/java/com/github/caoli5288/modernapi/ModernEntities.java
@@ -1,10 +1,19 @@
 package com.github.caoli5288.modernapi;
 
+import org.bukkit.entity.Damageable;
 import org.bukkit.entity.HumanEntity;
 
 public class ModernEntities {
 
     public static float getAttackCooldown(HumanEntity entity) {
         return entity.getAttackCooldown();
+    }
+
+    public static double getAbsorptionAmount(Damageable entity) {
+        return entity.getAbsorptionAmount();
+    }
+
+    public static void setAbsorptionAmount(Damageable entity, double amount) {
+        entity.setAbsorptionAmount(amount);
     }
 }
